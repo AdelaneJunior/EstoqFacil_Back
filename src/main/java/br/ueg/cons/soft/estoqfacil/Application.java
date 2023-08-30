01d24cdac400decc210acb71ed1f295e53773df5
lucashspiess <lucashs725@gmail.com>
2023-08-30 16:59:32 -0300

    teste

--- src/main/java/br/ueg/cons/soft/estoqfacil/Application.java
+++ src/main/java/br/ueg/cons/soft/estoqfacil/Application.java
@@ -22,5 +22,5 @@ public class Application {
 	}
 
 	//TODO resolver toda a questão do usuario e validar se os joinCollum estão funcionando, criar o resto das classes importantes
-
+	//TODO testar pull request automatico
 }
