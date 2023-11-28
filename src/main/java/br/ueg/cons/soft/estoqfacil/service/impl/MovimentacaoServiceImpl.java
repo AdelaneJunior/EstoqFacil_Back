ff8194e111b336283670c8df950911bff353b777
lucashspiess <lucashs725@gmail.com>
2023-11-28 20:46:26 -0300

    FIX validacoes de valores negativos
    Signed-off-by: lucashspiess <lucashs725@gmail.com>

--- src/main/java/br/ueg/cons/soft/estoqfacil/service/impl/MovimentacaoServiceImpl.java
+++ src/main/java/br/ueg/cons/soft/estoqfacil/service/impl/MovimentacaoServiceImpl.java
@@ -4,12 +4,15 @@ import br.ueg.cons.soft.estoqfacil.dto.RelatorioMovimentacaoDTO;
 import br.ueg.cons.soft.estoqfacil.model.Movimentacao;
 import br.ueg.cons.soft.estoqfacil.repository.MovimentacaoRepository;
 import br.ueg.cons.soft.estoqfacil.service.MovimentacaoService;
+import br.ueg.prog.webi.api.exception.ApiMessageCode;
+import br.ueg.prog.webi.api.exception.BusinessException;
+import br.ueg.prog.webi.api.exception.MessageCode;
 import br.ueg.prog.webi.api.service.BaseCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -30,7 +33,11 @@ public class MovimentacaoServiceImpl extends BaseCrudService<Movimentacao, Long,
 
     @Override
     protected void validarDados(Movimentacao entidade) {
-
+        if(entidade.getQuantidade() < 0 ||
+                entidade.getCusto().compareTo(BigDecimal.ONE) < 1 ||
+                entidade.getPreco().compareTo(BigDecimal.ONE) < 1){
+            throw new BusinessException(ApiMessageCode.ERRO_INESPERADO);
+        }
     }
 
     @Override
